912ccfbc76d03b242e5e9bcc1aa86358ff84e30c
shibayanmondal <shibayanmondal@gmail.com>
2022-03-04 17:14:47 +0530

    basics about js

---
+++ 1.hoisting/hoisting.js
@@ -0,0 +1,6 @@
+console.log(a)
+a = 10
+invoke()
+function invoke() {
+    console.log('Hello World')
+}
---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(window)
+console.log(this)
+document.getElementById('root').innerText="Hello World"
+document.getElementById('root').innerHTML="<h2>Hello World</h2>"
+
+
+function invoke() {
+    console.log('Hello World')
+}
+
+// document.getElementById('byClick').click()
